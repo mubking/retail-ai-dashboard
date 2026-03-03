9d6bfbf4a29b88b0b8417e126b91e39187de6e7e
Mubaraq <adeshinamubarak6@gmail.com>
2026-03-03 14:48:37 +0100

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import DashboardPage from './dashboard/page'
 
 function page() {
   return (
-    <div>page</div>
+      <div>
+          <DashboardPage/>
+    </div>
   )
 }
 
